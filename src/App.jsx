a9324e07c735f08c4f88807d938f0ee7648c4665
Orlando Verdin <git@orli.land>
2024-11-17 00:22:18 -0700

    add api request

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useQuery } from "react-query";
 import "./App.css";
 
 import IconDice from "./assets/icon-dice.svg";
@@ -5,21 +6,31 @@ import DividerDesktop from "./assets/pattern-divider-desktop.svg";
 import DividerMobile from "./assets/pattern-divider-mobile.svg";
 
 function App() {
+  const { data, isLoading, refetch } = useQuery("advice", async () => {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    return res.json();
+  });
+
+  if (isLoading) {
+    return (
+      <div className="spin">
+        <img src={IconDice} alt="loading new advice" />
+      </div>
+    );
+  }
+
   return (
     <div className="advice">
       <div className="advice__body">
-        <h1 className="advice__heading heading">ADVICE #117</h1>
-        <p className="advice__quote quote">
-          “It is easy to sit up and take notice, what's difficult is getting up
-          and taking action.”
-        </p>
+        <h1 className="advice__heading heading">ADVICE #{data.slip.id}</h1>
+        <p className="advice__quote quote">{data.slip.advice}</p>
 
         <picture>
           <source srcSet={DividerDesktop} media="(min-width: 572px)" />
           <img src={DividerMobile} alt="divider element" />
         </picture>
       </div>
-      <button className="advice__button">
+      <button className="advice__button" onClick={() => refetch()}>
         <img src={IconDice} alt="Generate new advice" />
       </button>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,15 @@ import { createRoot } from "react-dom/client";
 // Supports weights 200-800
 import "@fontsource-variable/manrope";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.jsx";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
